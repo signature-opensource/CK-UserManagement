436fee810080b8f40ebb5a1f9d389f84d6ae2d46
Dan Chiche <dan.chiche@signature.one>
2026-07-03 16:06:22 +0200

    Add UserManagementLocalePackage

---
+++ CK.UserManagement/UserManagementLocalePackage.cs
@@ -0,0 +1,13 @@
+using CK.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK.UserManagement;
+
+[Globalization.LocalePackage]
+public class UserManagementLocalePackage : IResourceGroup
+{
+}
